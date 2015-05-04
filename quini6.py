eb1abf6ebea80f5253824b81d7838d4f16002925
Ignacio Manzano <imanzano@tekgenesis.com>
2015-05-04 14:54:43 -0300

    apikey as parameter

--- quini6.py
+++ quini6.py
@@ -7,10 +7,15 @@ import json
 import csv
 
 parser = argparse.ArgumentParser(description='Process year and last lotery nro')
+parser.add_argument("apikey")
+parser.add_argument("",type=int)
 parser.add_argument("year",type=int)
 parser.add_argument("nro",type=int)
 args = parser.parse_args()
+
 year=args.year
+nro=args.nro
+apikey=args.apikey
 
 myfile = open("%s.csv" % year, 'wb')
 wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
@@ -18,11 +23,10 @@ wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
 before=datetime.datetime(year,1,1)
 after=datetime.datetime(year,12,31)
 rr = rrule.rrule(rrule.WEEKLY,byweekday=(relativedelta.SU,relativedelta.WE),dtstart=before)
-nro=args.nro
 
 for d in  reversed(rr.between(before,after,inc=True)):	
    date = d.strftime("%d-%m-%Y")
-   url = "https://www.kimonolabs.com/api/ondemand/a1cxw7t6?apikey=301f7a26396970ac0cf0a250d58958d8&kimpath2=sorteo-{}-del-dia-{}.htm".format(nro,date)
+   url = "https://www.kimonolabs.com/api/ondemand/a1cxw7t6?apikey={}&kimpath2=sorteo-{}-del-dia-{}.htm".format(apikey,nro,date)
    print url
    try:
       res= urllib2.urlopen(url)
