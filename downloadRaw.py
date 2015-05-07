c22b4479b1a984b61bd810807634ff22b05650f1
Ignacio Manzano <imanzano@tekgenesis.com>
2015-05-07 10:19:17 -0300

    Allow specify range

--- downloadRaw.py
+++ downloadRaw.py
@@ -1,12 +1,14 @@
 import urllib
+import argparse
 
-#parser = argparse.ArgumentParser(description='Process year and last lotery nro')
-#parser.add_argument("from",type=int)
-#parser.add_argument("to",type=int)
+parser = argparse.ArgumentParser(description='Download raw data')
+parser.add_argument("f",type=int)
+parser.add_argument("t",type=int)
 
-#args = parser.parse_args()
-
-for num in range(1279,2250):
+args = parser.parse_args()
+f=args.f
+t=args.t
+for num in range(f,t):
    url = "http://www.resultadosdelquini6.com/quini6/resultado-quini-6-{}".format(num)
    print url
    filename = "quini6-{}.html".format(num)
